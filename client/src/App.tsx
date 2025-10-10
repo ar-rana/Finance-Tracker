2923bb80ef5994cca6a57bb07588d2770124b1d8
Aryan Rana <aryan.rana2005@gmail.com>
2025-10-10 11:18:29 +0530

    instantiated react client with tailwind

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="p-1 m-2.5 text-2xl font-bold text-gray-400">
+        Client Instantiated
+      </h1>
+    </>
+  );
+}
+
+export default App;
